de5f8aa9802aeb29f23481be464bd40a969d061f
Long Giang <longgiang2015bn@gmail.com>
2021-01-12 23:09:40 +0700

    custom drawer

--- front_end/App_main_screen.js
+++ front_end/App_main_screen.js
@@ -1,17 +1,45 @@
 import React, {Component} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList} from '@react-navigation/drawer'
 import Category from './screen/Category'
 import App1 from './App1';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import Iconicons from 'react-native-vector-icons/Ionicons';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
+import { Body, Container, Header } from 'native-base';
+import { DrawerItems } from 'react-navigation-drawer';
 
 const Drawer = createDrawerNavigator();
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#23596e",
+    height: 150,
+    flexDirection: 'row'
+  },
+  text: {
+    color: 'white',
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'sans-serif-condensed'
+  }
+})
+
 export default class App_main_screen extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      account: ''
+    }
+  }
+
+  async componentDidMount(){
+    console.log(this.props);
+    let ac = await fetch('http://10.0.2.2:5000/get/user/' + this.props.route.params.userid);
+    ac = await ac.json();
+    this.setState({
+      account: ac.account
+    })
   }
 
   render() {
@@ -20,6 +48,12 @@ export default class App_main_screen extends Component {
       <Drawer.Navigator initialRouteName="App1" drawerContent={props => {
     return (
       <DrawerContentScrollView {...props}>
+        <View style ={styles.header}>
+          <Image source={require('./user.png')} style = {{marginLeft: 5, height: 100, width: 100, marginTop: 50, bottom: 5}}></Image>
+          <View style  = {{marginTop: 80, marginLeft: 10, marginBottom: 20}}>
+            <Text style = {styles.text}>{this.state.account.toUpperCase()}</Text>
+          </View>
+        </View>
         <DrawerItemList {...props} />
         <DrawerItem label="Logout" style = {{fontWeight: 'bold'}} onPress={() => this.props.navigation.navigate("Login")} />
       </DrawerContentScrollView>
